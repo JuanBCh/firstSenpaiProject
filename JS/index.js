011b0c9a77f79d1ba1cdae3cda3a0a4306dd0b48
Juan <jachiappara@gmail.com>
2022-07-07 15:26:16 -0300

    Finished the name input and email input functions

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,8 @@
 let closeModal = document.getElementById("modal_x");
 let openModal = document.getElementById("open_modal");
 let modalContainer = document.querySelectorAll(".modal_container")[0];
-let myCompany = document.getElementById("my_company");
-let myOrganization = document.getElementById("my_organization");
+let myCompanyBtn = document.getElementById("my_company");
+let myOrganizationBtn = document.getElementById("my_organization");
 
 const letsToggleModal = () => {
   modalContainer.classList.toggle("modal_close");
@@ -24,5 +24,40 @@ const choseOrganization = () => {
 
 openModal.addEventListener("click", letsToggleModal);
 closeModal.addEventListener("click", letsToggleModal);
-myCompany.addEventListener("click", choseCompany);
-myOrganization.addEventListener("click", choseOrganization);
+myCompanyBtn.addEventListener("click", choseCompany);
+myOrganizationBtn.addEventListener("click", choseOrganization);
+
+let nameInput = document.getElementById("name_input");
+let mailInput = document.getElementById("mail_input");
+let msjTextArea = document.getElementById("textarea");
+let formBtn = document.getElementById("form_btn");
+
+function useRegexForName(nameInput) {
+  let regex = /[^a-zA-Z]+/i;
+  return regex.test(nameInput);
+}
+const ValidateOnlyText = () => {
+  if (useRegexForName(nameInput.value) == true)
+    nameInput.classList.add("wrong_name");
+  else nameInput.classList.remove("wrong_name");
+};
+const onlyTextAlert = () => {
+  if (useRegexForName(nameInput.value) == true)
+    alert(`El campo de "nombre" solo se puede completar con letras.`);
+};
+
+function useRegexForMail(mailInput) {
+  let regex = /@\./i;
+  return regex.test(mailInput);
+}
+const validateMail = () => {
+  if (useRegexForName(mailInput.value) == false) {
+    mailInput.classList.add("wrong_name");
+    setTimeout(mailAlert, 100);
+  } else mailInput.classList.remove("wrong_name");
+};
+const mailAlert = () => alert(`El e-mail debe ser válido.`);
+
+nameInput.addEventListener("keydown", () => setTimeout(ValidateOnlyText, 1));
+nameInput.addEventListener("blur", () => setTimeout(onlyTextAlert, 1));
+mailInput.addEventListener("blur", () => setTimeout(validateMail, 1));
